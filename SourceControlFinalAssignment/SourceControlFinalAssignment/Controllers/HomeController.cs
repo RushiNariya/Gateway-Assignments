9a156f447b0ab57aaa5210132fbb433eee08f736
RushiNaria <52628468+RushiNaria@users.noreply.github.com>
2020-12-15 23:05:23 +0530

    Source Control Final Assignment
    Source Control Final Assignment with User registration, login, user details dashboard, Exception Handling.

---
+++ SourceControlFinalAssignment/SourceControlFinalAssignment/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNet.Identity;
+using SourceControlFinalAssignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SourceControlFinalAssignment.ExceptionHandling;
+
+namespace SourceControlFinalAssignment.Controllers
+{
+    [UserExceptionHandler]
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ApplicationDbContext dbContext = new ApplicationDbContext();
+            string email = User.Identity.GetUserName();
+            if (string.IsNullOrEmpty(email))
+            {
+                return RedirectToAction("Login", "Account");
+            }
+            var applicationUser = dbContext.Users.Where(x => x.Email.Equals(email)).FirstOrDefault();
+            ViewBag.User = applicationUser;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ SourceControlFinalAssignment/SourceControlFinalAssignment/ExceptionHandling/UserExceptionHandler.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using log4net;
+
+namespace SourceControlFinalAssignment.ExceptionHandling
+{
+    public class UserExceptionHandler : HandleErrorAttribute
+    {
+		//private static readonly ILog Log = LogManager.GetLogger(typeof(ProductExceptionHandler));
+
+		public override void OnException(ExceptionContext filterContext)
+		{
+			if (filterContext.Exception is NullReferenceException)
+			{
+				//Log.Error("NullReferanceException exception occured :" + filterContext.Exception.Message);
+				var controllerName = (string)filterContext.RouteData.Values["controller"];
+				var actionName = (string)filterContext.RouteData.Values["action"];
+
+				HandleErrorInfo model = new HandleErrorInfo(filterContext.Exception, controllerName, actionName);
+
+				filterContext.Result = new ViewResult
+				{
+					ViewName = "~/Views/Shared/Error.cshtml",
+					ViewData = new ViewDataDictionary<HandleErrorInfo>(model)
+				};
+
+				filterContext.ExceptionHandled = true;
+			}
+			else if (filterContext.Exception is NotImplementedException)
+			{
+				//Log.Error("NotImplementedException exception occured :" + filterContext.Exception.Message);
+				var controllerName = (string)filterContext.RouteData.Values["controller"];
+				var actionName = (string)filterContext.RouteData.Values["action"];
+
+				HandleErrorInfo model = new HandleErrorInfo(filterContext.Exception, controllerName, actionName);
+
+				filterContext.Result = new ViewResult
+				{
+					ViewName = "~/Views/Shared/Error.cshtml",
+					ViewData = new ViewDataDictionary<HandleErrorInfo>(model)
+				};
+
+				filterContext.ExceptionHandled = true;
+			}
+			else if (filterContext.Exception is InvalidOperationException)
+			{
+				//Log.Error("InvalidOperationException exception occured :" + filterContext.Exception.Message);
+				var controllerName = (string)filterContext.RouteData.Values["controller"];
+				var actionName = (string)filterContext.RouteData.Values["action"];
+
+				HandleErrorInfo model = new HandleErrorInfo(filterContext.Exception, controllerName, actionName);
+
+				filterContext.Result = new ViewResult
+				{
+					ViewName = "~/Views/Shared/Error.cshtml",
+					ViewData = new ViewDataDictionary<HandleErrorInfo>(model)
+				};
+
+				filterContext.ExceptionHandled = true;
+			}
+			else if (filterContext.Exception is Exception)
+			{
+				//Log.Error("Exception exception occured :" + filterContext.Exception.Message);
+				var controllerName = (string)filterContext.RouteData.Values["controller"];
+				var actionName = (string)filterContext.RouteData.Values["action"];
+
+				HandleErrorInfo model = new HandleErrorInfo(filterContext.Exception, controllerName, actionName);
+
+				filterContext.Result = new ViewResult
+				{
+					ViewName = "~/Views/Shared/Error.cshtml",
+					ViewData = new ViewDataDictionary<HandleErrorInfo>(model)
+				};
+
+				filterContext.ExceptionHandled = true;
+			}
+
+
+			//filterContext.HttpContext.IsCustomErrorEnabled
+			base.OnException(filterContext);
+		}
+	}
+}
