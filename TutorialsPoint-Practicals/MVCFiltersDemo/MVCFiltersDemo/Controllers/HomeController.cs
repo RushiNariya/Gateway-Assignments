712f374f1a537fb0087a9df37926734ceed4382f
RushiNaria <52628468+RushiNaria@users.noreply.github.com>
2021-01-13 11:08:45 +0530

    Added TutorialPoints Practicals
    Added TutorialPoints Practicals.

---
+++ TutorialsPoint-Practicals/MVCFiltersDemo/MVCFiltersDemo/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using MVCFiltersDemo.ActionFilters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCFiltersDemo.Controllers
+{
+    [MyLogActionFilter]
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        [OutputCache(Duration = 10)]
+        public string Index()
+        {
+            return "This is ASP.Net MVC Filters Tutorial";
+        }
+
+        [OutputCache(Duration = 10)]
+        public string GetCurrentTime()
+        {
+            return DateTime.Now.ToString("T");
+        }   
+    }
+}
---
+++ TutorialsPoint-Practicals/MVCSelectorsDemo/MVCSelectorsDemo/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCSelectorsDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "This is ASP.Net MVC Filters Tutorial";
+        }
+
+        #region ActionName Example (Selector)
+        //[ActionName("CurrentTime")]
+        //public string GetCurrentTime()
+        //{
+        //    return DateTime.Now.ToString("T");
+        //} 
+        #endregion
+
+        [ActionName("CurrentTime")]
+        public string GetCurrentTime()
+        {
+            return TimeString();
+        }
+
+        [NonAction]
+        public string TimeString()
+        {
+            return "Time is " + DateTime.Now.ToString("T");
+        }
+    }
+}
